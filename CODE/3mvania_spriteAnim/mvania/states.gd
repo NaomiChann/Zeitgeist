8edd52db016af75706a13d559b3d650860d06afc
Anna G <85114312+Annagmo@users.noreply.github.com>
2024-03-13 23:55:30 -0300

    Add files via upload

---
+++ CODE/3mvania_spriteAnim/mvania/states.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var idle = $Idle
+@onready var jump = $Jump
+@onready var move = $Move
+@onready var fall = $Fall
+@onready var slide = $Slide
+@onready var wallkick = $WallKick
+@onready var wallslide = $WallSlide
+
+func _ready():
+	pass
+
+func _process( delta ):
+	pass
